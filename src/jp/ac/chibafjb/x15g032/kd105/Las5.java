b4c6c3b48ad9b40b9e810887be465a0a23d64e4b
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-02 14:59:33 +0900

    課題５つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd105/Las5.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g032.kd105;
+
+public class Las5 {
+
+	public static void main(String[] args) {
+		System.out.println("氏名： M yuu");
+		System.out.println("趣味： ロードバイク サバイバルゲーム ゲーム実況");
+		System.out.println("目標： そに子のフィギュアコンプ");
+		
+
+	}
+
+}
