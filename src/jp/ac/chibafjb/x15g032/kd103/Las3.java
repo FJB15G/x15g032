ca5a0a411e6843aca4a5bf32527d44f4438557d0
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-02 14:58:58 +0900

    課題３つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd103/Las3.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g032.kd103;
+
+public class Las3 {
+
+	public static void main(String[] args) {
+		System.out.print("1 船橋太郎 ");
+		System.out.print("2 船橋太郎 ");
+		System.out.print("3 船橋太郎 ");
+		System.out.print("4 船橋太郎 "); 
+		System.out.print("5 船橋太郎 ");
+		System.out.print("6 船橋太郎 ");
+		System.out.print("7 船橋太郎 ");
+		System.out.print("8 船橋太郎 ");
+		System.out.print("9 船橋太郎 ");
+		System.out.print("10船橋太郎\n");
+	}
+
+}
