6aa8fc5691ac8290de58d24581ec2a3b709b3d39
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-02 14:39:54 +0900

    課題２つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd102/Las2.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g032.kd102;
+
+public class Las2 {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋太郎");
+		System.out.println("2 船橋太郎");
+		System.out.println("3 船橋太郎");
+		System.out.println("4 船橋太郎");
+		System.out.println("5 船橋太郎");
+		System.out.println("6 船橋太郎");
+		System.out.println("7 船橋太郎");
+		System.out.println("8 船橋太郎");
+		System.out.println("9 船橋太郎");
+		System.out.println("10船橋太郎");
+	
+
+	}
+
+}
