352de06daaf7df11617aded1089fd093de756d98
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-02 14:25:42 +0900

    課題１つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd101/Las.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g032.kd101;
+
+public class Las {
+
+	public static void main(String[] args) {
+		System.out.println("船橋情報ビジネス専門学校");
+		System.out.println("047-425-1051");
+		System.out.println("Hello☆こんにちは");
+		
+
+	}
+
+}
