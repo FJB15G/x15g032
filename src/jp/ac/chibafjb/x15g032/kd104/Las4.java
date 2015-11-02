cbeaa3e18aaa7b2444f1b72b5bb244c316d2a31f
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-02 14:59:15 +0900

    課題４つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd104/Las4.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g032.kd104;
+
+public class Las4 {
+
+	public static void main(String[] args) {
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお\n");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお\n");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお\n");
+
+	}
+
+}
