d3242a7578b291a9dce8f7d3f950adbf24e55cfc
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-02 14:59:47 +0900

    課題６つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd106/Las6.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g032.kd106;
+
+public class Las6 {
+
+	public static void main(String[] args) {
+		System.out.println("Λ   Λ");
+		System.out.println("( ･｜･)");
+		System.out.println("｜   ｜");
+		System.out.println("｜ ○｜");
+		System.out.println("(__)(__) ");
+
+	}
+
+}
