91291caefcde80bc235566635dccaef62ddd516e
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-09 14:06:51 +0900

    課題８つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd112/Las8.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g032.kd112;
+
+public class Las8 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<99;i++)
+			System.out.println("船橋情報ビジネス専門学校");
+
+	}
+
+}
