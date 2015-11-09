274611407bc2c32c9a59f4a3a593b8863cb6ceb0
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-09 15:10:22 +0900

    課題２４こめ

---
+++ src/jp/ac/chibafjb/x15g032/kd133/Las24.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g032.kd133;
+
+public class Las24 {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		x = 5;
+		y = 10;
+		System.out.println("x+y="+15);
+
+	}
+
+}
