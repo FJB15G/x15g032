c792c926a8b28bba0eac0340b5255e855b69e851
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-09 14:19:23 +0900

    課題11つめ

---
+++ src/jp/ac/chibafjb/x15g032/kd115/Las11.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g032.kd115;
+
+public class Las11 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<2;i++)
+			System.out.println("あいうえお あいうえお あいうえお");
+		for(i=0;i<2;i++)
+			System.out.println("かきくけこ かきくけこ かきくけこ");
+		for(i=0;i<2;i++)
+			System.out.println("さしすせそ さしすせそ さしすせそ");
+
+
+	}
+
+}
