f9059df24c72cea90746b767bb825039b336cd84
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-09 14:27:53 +0900

    課題１２こめ

---
+++ src/jp/ac/chibafjb/x15g032/kd116/Las12.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g032.kd116;
+
+public class Las12 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<2;i++)
+			System.out.print("あいうえお あいうえお あいうえお\n"+"かきくけこ かきくけこ かきくけこ\n"+"さしすせそ さしすせそ さしすせそ\n");
+		
+	}
+
+}
