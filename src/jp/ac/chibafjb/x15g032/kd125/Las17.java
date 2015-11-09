98ce5685069d7eae0e7470a15e67c0f8db8f35d1
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-09 14:41:07 +0900

    課題１７こめ

---
+++ src/jp/ac/chibafjb/x15g032/kd125/Las17.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g032.kd125;
+
+public class Las17 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=10;i<=30;i=i+10)
+		System.out.println(i+"*");
+
+	}
+
+}
