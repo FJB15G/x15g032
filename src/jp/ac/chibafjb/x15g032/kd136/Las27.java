82987150a9f14f2f4d485bd46e3d492813ecdda5
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-16 14:11:13 +0900

    課題27個目

---
+++ src/jp/ac/chibafjb/x15g032/kd136/Las27.java
@@ -0,0 +1,21 @@
+package jp.ac.chibafjb.x15g032.kd136;
+
+public class Las27 {
+
+	public static void main(String[] args) {
+	    int a;
+	    int b;
+	    int c;
+	    a = 100;
+	    b = 1;
+	    c = 0;
+	    c = b;
+	    b = a;
+	    a = c;
+	    System.out.println("a ="+a);
+	    System.out.println("b ="+b);
+	    System.out.println("c ="+c);
+
+	}
+
+}
