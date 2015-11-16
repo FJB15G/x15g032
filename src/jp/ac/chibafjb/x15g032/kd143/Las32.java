1079b3800f7de8bce76825236a749f66973bdd11
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-16 15:10:46 +0900

    課題32個目

---
+++ src/jp/ac/chibafjb/x15g032/kd143/Las32.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g032.kd143;
+
+import java.util.Scanner;
+
+public class Las32 {
+
+	public static void main(String[] args) {
+		Scanner sin =new Scanner(System.in);
+		String s1 =sin.next();
+		String s2 =sin.next();
+		String s3 =sin.next();
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+
+	}
+
+}
