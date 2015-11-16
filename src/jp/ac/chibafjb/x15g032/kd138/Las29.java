f552412f79c1f61d9d0a7751d35039ab4f398266
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-16 14:18:59 +0900

    課題29個目

---
+++ src/jp/ac/chibafjb/x15g032/kd138/Las29.java
@@ -0,0 +1,25 @@
+package jp.ac.chibafjb.x15g032.kd138;
+
+public class Las29 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		int d;
+		a =1;
+		b =2;
+		c =3;
+		d =0;
+		d =c;
+		c =a;
+		a =b;
+		b =d;
+		
+		System.out.println("a ="+a);
+		System.out.println("b ="+b);
+		System.out.println("c ="+c);
+
+	}
+
+}
