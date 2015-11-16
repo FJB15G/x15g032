6c04fd174e9a549bf0b57e89bfb2b0484a438f2e
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-16 14:05:32 +0900

    課題26個目

---
+++ src/jp/ac/chibafjb/x15g032/kd135/Las26.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g032.kd135;
+
+import java.util.Scanner;
+
+public class Las26 {
+
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		String str1;
+		String str2;
+		str1 = "apple";
+		str2 = "orange";
+		System.out.println("str1 ="+str1);
+		System.out.println("str2 ="+str2);
+		
+		
+
+	}
+
+}
