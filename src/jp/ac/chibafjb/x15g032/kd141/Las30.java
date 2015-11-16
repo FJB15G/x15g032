823a083088ff7fe24a4bbb6224f46cc921025fe8
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-16 14:31:31 +0900

    課題30個目

---
+++ src/jp/ac/chibafjb/x15g032/kd141/Las30.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g032.kd141;
+
+import java.util.Scanner;
+
+public class Las30 {
+
+	public static void main(String[] args) {
+		Scanner sin =new Scanner(System.in);
+		String s1 = sin.next();
+		sin.close();
+		
+		System.out.println("整数を一つ入力してください。  ――→"+s1);
+		System.out.println("あなたの入力した数は"+s1+"です。");
+
+	}
+
+}
