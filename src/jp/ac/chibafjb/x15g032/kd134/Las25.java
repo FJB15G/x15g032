012ebfe6d62bb9e729e677983ee47d5d49eb6adc
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-16 13:53:59 +0900

    課題２５こめ

---
+++ src/jp/ac/chibafjb/x15g032/kd134/Las25.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g032.kd134;
+
+public class Las25 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		
+		y = x;
+		
+		System.out.println("x ="+x);
+		System.out.println("y ="+y);
+
+	}
+
+}
