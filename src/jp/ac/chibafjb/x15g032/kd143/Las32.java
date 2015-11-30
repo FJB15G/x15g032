24d9c3ee8ef58a6e3d617096195b99bf758bbdf4
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-11-30 15:05:32 +0900

    課題33個目

--- src/jp/ac/chibafjb/x15g032/kd143/Las32.java
+++ src/jp/ac/chibafjb/x15g032/kd143/Las32.java
@@ -6,10 +6,14 @@ public class Las32 {
 
 	public static void main(String[] args) {
 		Scanner sin =new Scanner(System.in);
+		System.out.print("住所の入力 -->");
 		String s1 =sin.next();
+		System.out.print("氏名の入力 -->");
 		String s2 =sin.next();
+		System.out.print("電話番号の入力 -->");
 		String s3 =sin.next();
-		
+		sin.close();
+	
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
