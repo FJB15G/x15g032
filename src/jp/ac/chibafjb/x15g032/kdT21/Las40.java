33213cd2c1e728604125404628c750bccab6031e
unknown <x15g032@PC504N002.chiba-fjb.ac.jp>
2015-12-14 15:08:06 +0900

    課題42個目

--- src/jp/ac/chibafjb/x15g032/kdT21/Las40.java
+++ src/jp/ac/chibafjb/x15g032/kdT21/Las40.java
@@ -1,4 +1,4 @@
-package jp.ac.chibafjb.x15g032.kdT14;
+package jp.ac.chibafjb.x15g032.kdT21;
 
 import java.util.Scanner;
 
@@ -10,13 +10,18 @@ public class Las40 {
 		int h = sin.nextInt();
 		System.out.print("フライドポテトの個数  -->");
 		int f = sin.nextInt();
-		sin.close();
 		
 		System.out.println("ハンバーガーの代金  = "+(100*h)+"円");
 		System.out.println("フライドポテト      = "+(180*f)+"円");
 		System.out.println("小計                = "+((100*h)+(180*f))+"円");
 		System.out.println("");
 		System.out.println("消費税              ="+(int)(Math.floor((100*h)+(180*f))*0.08)/1+"円");
+		System.out.println("合計金額            ="+(int)(Math.floor((100*h)+(180*f))*1.08)/1+"円");
+		System.out.println("");
+		System.out.print("受取金額             -->");
+		int k = sin.nextInt();
+		System.out.println("つり銭              ="+(k-(int)(Math.floor((100*h)+(180*f))*1.08)/1+"円"));
+		sin.close();
 
 	}
 
